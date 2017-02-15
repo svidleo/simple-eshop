327dc6aa9053c70cf9c5c217fe788ab5ca1caf46
Julius Burbulis <burbulis.julius@gmail.com>
2017-02-15 13:12:17 +0200

    [Dev] Added CORS filter

--- src/main/java/com/swedbank/itacademy/eshop/controller/ShopController.java
+++ src/main/java/com/swedbank/itacademy/eshop/controller/ShopController.java
@@ -7,11 +7,9 @@ import com.swedbank.itacademy.eshop.repository.CategoriesRepository;
 import com.swedbank.itacademy.eshop.repository.OrdersRepository;
 import com.swedbank.itacademy.eshop.repository.ProductsRespository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class ShopController {
 
